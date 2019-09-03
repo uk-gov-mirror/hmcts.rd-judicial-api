8615197bc70c86a286311ad4224e354c30bc127e
Kotla <prashanth.kotla@hmcts.net>
2019-09-03 15:36:28 +0100

    judicial microservice project created

---
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/JudicialApplication.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.judicialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableJpaAuditing
+@EnableJpaRepositories
+@EnableRetry
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableFeignClients(basePackages = {
+        "uk.gov.hmcts.reform.judicialapi",
+})
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+public class JudicialApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(JudicialApplication.class, args);
+    }
+}
