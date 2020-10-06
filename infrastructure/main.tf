dc7073c4dbec9692b4bb70c8d38ce672d9458802
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2020-10-06 13:25:29 +0100

    TF 0.12.2 syntax updates and clean-up - Master (#70)
    * Upgrade TF to 0.12.16 and Azure RM to 2.20.0
    
    * Fix syntax errors

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,84 +1,52 @@
 locals {
-  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  preview_app_service_plan = "${var.product}-${var.component}-${var.env}"
-  non_preview_app_service_plan = "${var.product}-${var.env}"
-  app_service_plan = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
-
-  preview_vault_name = "${var.raw_product}-aat"
-  non_preview_vault_name = "${var.raw_product}-${var.env}"
-  key_vault_name = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
-
-  s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  s2s_vault_name = "s2s-${local.local_env}"
-  s2s_vault_uri = "https://s2s-${local.local_env}.vault.azure.net/"
-
+  preview_vault_name      = join("-", [var.raw_product, "aat"])
+  non_preview_vault_name  = join("-", [var.raw_product, var.env])
+  key_vault_name          = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
 }
 
 data "azurerm_key_vault" "rd_key_vault" {
-  name = "${local.key_vault_name}"
-  resource_group_name = "${local.key_vault_name}"
+  name                = local.key_vault_name
+  resource_group_name = local.key_vault_name
 }
-
-data "azurerm_key_vault" "s2s_key_vault" {
-  name = "s2s-${local.local_env}"
-  resource_group_name = "rpe-service-auth-provider-${local.local_env}"
-}
-
-data "azurerm_key_vault_secret" "s2s_microservice" {
-  name = "s2s-microservice"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
-
-data "azurerm_key_vault_secret" "s2s_url" {
-  name = "s2s-url"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
-
-
-data "azurerm_key_vault_secret" "s2s_secret" {
-  name = "microservicekey-rd-judicial-api"
-  key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
-}
-
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name      = "${var.component}-POSTGRES-USER"
-  value     = "${module.db-judicial-ref-data.user_name}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-USER"])
+  value         = module.db-judicial-ref-data.user_name
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name      = "${var.component}-POSTGRES-PASS"
-  value     = "${module.db-judicial-ref-data.postgresql_password}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-PASS"])
+  value         = module.db-judicial-ref-data.postgresql_password
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name      = "${var.component}-POSTGRES-HOST"
-  value     = "${module.db-judicial-ref-data.host_name}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-HOST"])
+  value         = module.db-judicial-ref-data.host_name
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name      = "${var.component}-POSTGRES-PORT"
-  value     = "5432"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-PORT"])
+  value         = "5432"
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name      = "${var.component}-POSTGRES-DATABASE"
-  value     = "${module.db-judicial-ref-data.postgresql_database}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-DATABASE"])
+  value         = module.db-judicial-ref-data.postgresql_database
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 module "db-judicial-ref-data" {
-  source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product = "${var.product}-${var.component}-postgres-db"
-  location = "${var.location}"
-  subscription = "${var.subscription}"
-  env = "${var.env}"
-  postgresql_user = "dbjuddata"
-  database_name = "dbjuddata"
-  common_tags = "${var.common_tags}"
-  postgresql_version    = "${var.postgresql_version}"
+  source              = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product             = join("-", [var.product, var.component, "postgres-db"])
+  location            = var.location
+  subscription        = var.subscription
+  env                 = var.env
+  postgresql_user     = "dbjuddata"
+  database_name       = "dbjuddata"
+  common_tags         = var.common_tags
+  postgresql_version  = var.postgresql_version
 }
