b6f0643c1f6851f0c9757fbd8e8dc003941cc0cd
shivajihmcts <89258035+shivajihmcts@users.noreply.github.com>
2022-06-08 07:22:29 +0100

    Feat:To handle 500 HTTP responses (#304)
    * RDCC-4680:Changes made to addfilter and exception handling for  500 error.
    
    * RDCC-4680:Changes made to incorporate review comment
    
    * RDCC-4680:Changes made to incorporate review comment
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/judicialapi/controller/repository/IdamRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/judicialapi/controller/repository/IdamRepositoryTest.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.reform.judicialapi.controller.repository;
 
 import com.github.benmanes.caffeine.cache.Cache;
+import feign.FeignException;
+import feign.Request;
+import feign.RequestTemplate;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,12 +14,17 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+import uk.gov.hmcts.reform.judicialapi.controller.advice.UnauthorizedException;
 import uk.gov.hmcts.reform.judicialapi.repository.IdamRepository;
 
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -51,5 +60,32 @@ class IdamRepositoryTest {
         verify(cacheManager, times(1)).getCache(any());
         verify(caffeineCacheMock, times(1)).getNativeCache();
         verify(cache, times(1)).estimatedSize();
+    }
+
+    @Test
+    void test_getUserInfo_unAuthorizedException() {
+
+        UserInfo userInfo = mock(UserInfo.class);
+        CaffeineCache caffeineCacheMock = mock(CaffeineCache.class);
+        Cache cache = mock(Cache.class);
+
+        Request request = Request.create(Request.HttpMethod.GET, "url",
+                new HashMap<>(), null, new RequestTemplate());
+        when(idamClient.getUserInfo(anyString()))
+                .thenThrow(new FeignException
+                        .Unauthorized("User is not authorized", request,null,null));
+
+        when(cacheManager.getCache(anyString())).thenReturn(caffeineCacheMock);
+        doReturn(cache).when(caffeineCacheMock).getNativeCache();
+        when(cache.estimatedSize()).thenReturn(anyLong());
+
+        UnauthorizedException thrown = Assertions.assertThrows(UnauthorizedException.class, () -> {
+            UserInfo returnedUserInfo = idamRepository.getUserInfo("Test");
+        });
+
+
+        assertThat(thrown.getMessage()).contains("User is not authorized");
+
+
     }
 }
