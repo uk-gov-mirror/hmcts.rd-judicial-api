b221a880cfb29910058e3ce521bc66d96f64a419
nayeemshaik-hmcts <nayeem.shaik@hmcts.net>
2022-12-19 14:40:35 +0000

    Changes required for JRD topic publish

--- src/main/resources/db/migration/V1_49__elinks_alter_dataload_schedular_audit.sql
+++ src/main/resources/db/migration/V1_49__elinks_alter_dataload_schedular_audit.sql
@@ -1,2 +1,12 @@
--- dbjudicialdata.dataload_schedular_audit  column name change
-ALTER TABLE dbjudicialdata.dataload_schedular_audit RENAME COLUMN file_name TO api_name;
+
+DROP TABLE dbjudicialdata.dataload_schedular_audit;
+
+CREATE TABLE dbjudicialdata.dataload_schedular_audit (
+	id serial NOT NULL,
+	scheduler_name varchar(64) NOT NULL,
+	scheduler_start_time timestamp NOT NULL,
+	scheduler_end_time timestamp NULL,
+	status varchar(32) NULL,
+	api_name varchar(128) NULL,
+	CONSTRAINT dataload_schedular_audit_pk PRIMARY KEY (id)
+);
