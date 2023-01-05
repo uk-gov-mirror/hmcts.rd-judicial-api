d562d86226563d4e78d0857270f92b1bbf182f1f
prudhvi-maddineni <111352154+prudhvi-maddineni@users.noreply.github.com>
2023-01-05 13:05:11 +0000

    audit utility method for updating dataload_schedular_audit table (#401)

---
+++ src/test/java/uk/gov/hmcts/reform/judicialapi/elinks/domain/ElinkDataSchedularAuditTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.judicialapi.elinks.domain;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+class ElinkDataSchedularAuditTest {
+
+    @Test
+    void testSchedularAudit() {
+        ElinkDataSchedularAudit schedularAudit = new ElinkDataSchedularAudit();
+        schedularAudit.setId(1);
+        schedularAudit.setSchedulerName("Test User");
+        schedularAudit.setSchedulerStartTime(LocalDateTime.now());
+        schedularAudit.setSchedulerEndTime(LocalDateTime.now());
+        schedularAudit.setStatus("SUCCESS");
+        schedularAudit.setApiName("BaseLocations");
+
+        assertNotNull(schedularAudit);
+        assertThat(schedularAudit.getId(), is(1));
+        assertThat(schedularAudit.getSchedulerName(), is("Test User"));
+        assertNotNull(schedularAudit.getSchedulerStartTime());
+        assertNotNull(schedularAudit.getSchedulerEndTime());
+        assertThat(schedularAudit.getStatus(), is("SUCCESS"));
+        assertThat(schedularAudit.getApiName(), is("BaseLocations"));
+    }
+
+}
