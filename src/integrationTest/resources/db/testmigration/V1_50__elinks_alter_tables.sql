63bc95b6b70dbbee2a9c883f950149c11615569d
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-01-24 12:30:16 +0000

    development code to exclude appointments having base location ids not present in baselocation table

--- src/integrationTest/resources/db/testmigration/V1_50__elinks_alter_tables.sql
+++ src/integrationTest/resources/db/testmigration/V1_50__elinks_alter_tables.sql
@@ -30,3 +30,7 @@ BY dbjudicialdata.dataload_schedular_audit.id;
 
 ALTER TABLE dbjudicialdata.dataload_schedular_audit
 ALTER COLUMN id SET DEFAULT nextval('dbjudicialdata.elink_audit_scheduler_id_sequence');
+
+
+insert into dbjudicialdata.region_type(region_id,region_desc_en,region_desc_cy)
+values ('0', 'default', 'default');
