6b8a3c4e161025270ccbf7fdbfde01608345ce9e
shivajihmcts <shivaji.gaikwad@hmcts.net>
2023-01-26 13:38:26 +0000

    RDCC-6084:Changes made to merge code from dev branch

--- src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/ElinksController.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/ElinksController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
