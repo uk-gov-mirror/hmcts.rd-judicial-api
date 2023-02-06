7d6450c1133fcef56016036ce018cb4e3a537f51
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-02-06 11:29:41 +0000

    [RDCC-6133]We need to exclude appointment for base_location not found in static table

--- src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/ElinksPeopleServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/ElinksPeopleServiceImpl.java
@@ -369,4 +369,5 @@ public class ElinksPeopleServiceImpl implements ElinksPeopleService {
             throw new ElinksException(HttpStatus.FORBIDDEN, ELINKS_ACCESS_ERROR, ELINKS_ACCESS_ERROR);
         }
     }
+
 }
