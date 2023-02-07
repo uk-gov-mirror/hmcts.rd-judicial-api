872a274c0cf5bb3f055e046a3aa9ae6a3fbfe54b
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-02-07 18:33:44 +0000

    [RDCC-6133]We need to exclude appointment for base_location not found in static table

---
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/domain/ElinkDataExceptionRecords.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.reform.judicialapi.elinks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity(name = "dataloadExceptionRecords")
+@Table(name = "dataload_exception_records", schema = "dbjudicialdata")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SequenceGenerator(name = "elink_exception_records_id_sequence",
+        sequenceName = "elink_exception_records_id_sequence",  schema = "dbjudicialdata", allocationSize = 1)
+public class ElinkDataExceptionRecords implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "elink_exception_records_id_sequence")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "scheduler_name")
+    private String schedulerName;
+
+    @Column(name = "scheduler_start_time")
+    private LocalDateTime schedulerStartTime;
+
+    @Column(name = "table_name")
+    private String tableName;
+
+    @Column(name = "key")
+    private String key;
+
+    @Column(name = "field_in_error")
+    private String fieldInError;
+
+    @Column(name = "error_description")
+    private String errorDescription;
+
+    @Column(name = "updated_timestamp")
+    private LocalDateTime updatedTimeStamp;
+
+    @Column(name = "row_id")
+    private int rowId;
+
+}
