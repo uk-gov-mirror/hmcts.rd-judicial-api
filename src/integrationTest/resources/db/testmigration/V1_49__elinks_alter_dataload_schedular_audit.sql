a6ad42984a19a9a534490dda84d2f6785877fa8f
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-02-07 22:34:32 +0000

    [RDCC-6133]We need to exclude appointment for base_location not found in static table

--- src/integrationTest/resources/db/testmigration/V1_49__elinks_alter_dataload_schedular_audit.sql
+++ src/integrationTest/resources/db/testmigration/V1_49__elinks_alter_dataload_schedular_audit.sql
@@ -16,3 +16,16 @@ CREATE TABLE dbjudicialdata.dataload_schedular_job(
     job_end_time TIMESTAMP,
     CONSTRAINT dataload_schedular_job_pk PRIMARY KEY (id)
 );
+
+CREATE TABLE dbjudicialdata.dataload_exception_records (
+	id serial4 NOT NULL,
+	table_name varchar(64) NULL,
+	scheduler_start_time timestamp NOT NULL,
+	scheduler_name varchar(64) NOT NULL,
+	"key" varchar(256) NULL,
+	field_in_error varchar(256) NULL,
+	error_description varchar(512) NULL,
+	updated_timestamp timestamp NOT NULL,
+	row_id int8 NULL,
+	CONSTRAINT dataload_exception_records_pk PRIMARY KEY (id)
+);
