dd5f1a6916084e95915a03a1677841a0b590cf02
hmcts <prudhvi.maddineni@hmcts.net>
2023-03-14 13:30:53 +0000

    flyway scripts creations

--- src/main/resources/db/migration/V1_52__elinks_alter_tables.sql
+++ src/main/resources/db/migration/V1_52__elinks_alter_tables.sql
@@ -16,21 +16,15 @@ ALTER COLUMN judicial_office_appointment_id SET DEFAULT nextval('dbjudicialdata.
 insert into dbjudicialdata.region_type(region_id,region_desc_en,region_desc_cy)
 values ('0', 'default', 'default');
 
-CREATE TABLE dbjudicialdata.judicial_location_mapping (
-	epimms_id varchar(16),
-	judicial_base_location_id varchar(64),
-	building_location_name varchar(256),
-	base_location_name varchar(128),
-	service_code varchar(16)
-);
-
 
 CREATE SEQUENCE dbjudicialdata.elink_audit_scheduler_id_sequence AS integer START 1 OWNED
 BY dbjudicialdata.dataload_schedular_audit.id;
 
 
 ALTER TABLE dbjudicialdata.dataload_schedular_audit
+
 ALTER COLUMN id SET DEFAULT nextval('dbjudicialdata.elink_audit_scheduler_id_sequence');
+
 
 CREATE SEQUENCE dbjudicialdata.elink_exception_records_id_sequence AS integer START 1 OWNED
 BY dbjudicialdata.dataload_exception_records.id;
