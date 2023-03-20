d6b064af399db9fa50f0a3b9b8b1c5a397faccfb
prudhvi-maddineni <111352154+prudhvi-maddineni@users.noreply.github.com>
2023-03-20 12:43:34 +0000

    flyway changes (#524)
    * flyway changes

--- src/integrationTest/resources/db/testmigration/V1_49__elinks_alter_tables.sql
+++ src/integrationTest/resources/db/testmigration/V1_49__elinks_alter_tables.sql
@@ -1,9 +1,28 @@
+--Require to modify flywayscript to run integration test cases
 
---Drop tables
-DROP TABLE dbjudicialdata.judicial_role_type;
-DROP TABLE dbjudicialdata.judicial_office_authorisation;
-DROP TABLE dbjudicialdata.judicial_office_appointment;
-DROP TABLE dbjudicialdata.judicial_user_profile;
+DROP SCHEMA if EXISTS dbjudicialdata CASCADE;
+
+CREATE SCHEMA IF NOT EXISTS dbjudicialdata;
+
+
+
+-- Create table script : base_location_type
+CREATE TABLE dbjudicialdata.base_location_type (
+	base_location_id varchar(64) NOT NULL,
+	court_name varchar(128) NULL,
+	court_type varchar(128) NULL,
+	circuit varchar(128) NULL,
+	area_of_expertise varchar(128) NULL,
+	CONSTRAINT base_location_id PRIMARY KEY (base_location_id)
+);
+
+-- Create table script : region_type
+CREATE TABLE dbjudicialdata.region_type (
+	region_id varchar(64) NOT NULL,
+	region_desc_en varchar(256) NOT NULL,
+	region_desc_cy varchar(256) NULL,
+	CONSTRAINT region_id PRIMARY KEY (region_id)
+);
 
 
 
@@ -62,8 +81,8 @@ CREATE TABLE dbjudicialdata.judicial_office_appointment (
 	base_location_id varchar(64) NULL,
 	region_id varchar(256) NULL,
 	is_prinicple_appointment boolean NULL,
-	start_date date NULL,
-	end_date date NULL,
+	start_date timestamp NULL,
+	end_date timestamp NULL,
 	created_date timestamp NULL,
 	last_loaded_date timestamp NULL,
 	epimms_id varchar(16) NULL,
