4d3e58ea6f0c462ae7bd99ad3347bc12b6bf9db7
arshinsalim <arshin.salim@hmcts.net>
2023-07-03 13:31:44 +0100

    sync between dev-elinks and master

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/util/KeyGenUtil.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/util/KeyGenUtil.java
@@ -33,8 +33,8 @@ public class KeyGenUtil {
 
     public static String getDynamicJwksResponse() throws JOSEException, JsonProcessingException {
         RSAKey rsaKey = KeyGenUtil.getRsaJwk();
-        Map<String, List<JSONObject>> body = new LinkedHashMap<>();
-        List<JSONObject> keyList = new ArrayList<>();
+        Map<String, List<Map<String, Object>>> body = new LinkedHashMap<>();
+        List<Map<String, Object>> keyList = new ArrayList<>();
         keyList.add(rsaKey.toJSONObject());
         body.put("keys", keyList);
         ObjectMapper objectMapper = new ObjectMapper();
