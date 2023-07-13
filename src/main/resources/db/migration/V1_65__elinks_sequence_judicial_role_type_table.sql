b79fff943066d9fb0e0f1e7be126dbd71c62e5ec
arshinsalim <arshin.salim@hmcts.net>
2023-07-13 11:09:13 +0100

    mapping from elinks to db

--- src/main/resources/db/migration/V1_65__elinks_sequence_judicial_role_type_table.sql
+++ src/main/resources/db/migration/V1_65__elinks_sequence_judicial_role_type_table.sql
@@ -1,4 +1,4 @@
-
+-- update row script : dbjudicialdata.jrd_lrd_region_mapping
 
 CREATE SEQUENCE dbjudicialdata.judicial_role_type_id_sequence AS integer START 1 OWNED
 BY dbjudicialdata.judicial_role_type.role_id;
