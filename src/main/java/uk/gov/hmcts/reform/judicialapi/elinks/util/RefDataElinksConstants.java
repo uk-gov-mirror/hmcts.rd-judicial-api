ed4437b83861b40d26479b9223a14e7dac31ddf2
sahitya-desireddy <93707379+sahitya-desireddy@users.noreply.github.com>
2023-08-17 13:48:38 +0100

    Rdcc 6984 (#618)
    * integration test case
    
    * integration test case
    
    * integration test case
    
    * integration test case

--- src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/util/RefDataElinksConstants.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/util/RefDataElinksConstants.java
@@ -97,7 +97,7 @@ public class RefDataElinksConstants {
     public static final String APPOINTMENTIDNOTAVAILABLE = "Appointment  ID : "
         + "is not available in Appointment Table";
 
-    public static final String APPOINTMENTID_IS_NULL = "Appointment  ID : "
+    public static final String APPOINTMENTID_IS_NULL = "Appointment ID: "
             + "is Null for the given Authorisation";
 
     public static final String USERPROFILEEMAILID = "Personal Code : "
