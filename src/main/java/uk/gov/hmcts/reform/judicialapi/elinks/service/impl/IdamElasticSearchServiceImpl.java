90aafbe28ffea96dfe6a8e33b54ca36d46fe5b53
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-09-12 15:46:56 +0100

    mismatched object ids in to exception table

--- src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/IdamElasticSearchServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/IdamElasticSearchServiceImpl.java
@@ -179,16 +179,16 @@ public class IdamElasticSearchServiceImpl implements IdamElasticSearchService {
                 .filter(entry -> !jrdObjectIdsList.contains(entry.getKey()))
                 .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
 
-        String errorDescription = "Received from Idam is not present in Judicial Reference Data";
+        String errorDescription = " Received from Idam is not present in Judicial Reference Data";
 
         LocalDateTime schedulerStartTime = now();
 
-        if(!filteredObjectIds.isEmpty()) {
+        if (!filteredObjectIds.isEmpty()) {
             for (var entry : filteredObjectIds.entrySet()) {
                 elinkDataExceptionHelper.auditException(JUDICIAL_REF_DATA_ELINKS,
                         schedulerStartTime,
-                        entry.getValue(),
-                        OBJECT_ID, entry.getValue() + errorDescription, USER_PROFILE, entry.getKey());
+                        entry.getKey(),
+                        OBJECT_ID, entry.getKey() + errorDescription, USER_PROFILE, entry.getValue());
             }
         }
     }
