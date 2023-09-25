22080c032619b1962dc7c889104e420b8e93a77b
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-09-25 11:31:45 +0100

    sonar coverage for helper test

---
+++ src/test/java/uk/gov/hmcts/reform/judicialapi/elinks/util/ElinksResponsesHelperTest.java
@@ -0,0 +1,111 @@
+package uk.gov.hmcts.reform.judicialapi.elinks.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Request;
+import feign.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.judicialapi.elinks.repository.ElinksResponsesRepository;
+import uk.gov.hmcts.reform.judicialapi.elinks.response.BaseLocationResponse;
+import uk.gov.hmcts.reform.judicialapi.elinks.response.ElinkBaseLocationResponse;
+
+import java.util.List;
+
+import static java.nio.charset.Charset.defaultCharset;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.judicialapi.elinks.util.RefDataElinksConstants.LOCATION;
+
+@ExtendWith(MockitoExtension.class)
+public class ElinksResponsesHelperTest {
+
+    @Spy
+    private ElinksResponsesRepository elinksResponsesRepository;
+
+    @Mock
+    ElinksResponsesHelper elinksResponsesHelper;
+
+    @Test
+    void saveElinksResponses() throws JsonProcessingException {
+
+        List<BaseLocationResponse> baseLocations = getBaseLocationResponseData();
+
+        ElinkBaseLocationResponse elinkBaseLocationResponse = new ElinkBaseLocationResponse();
+        elinkBaseLocationResponse.setResults(baseLocations);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String body = mapper.writeValueAsString(elinkBaseLocationResponse);
+
+        elinksResponsesHelper.saveElinksResponse(LOCATION, Response.builder()
+                .request(mock(Request.class)).body(body, defaultCharset()).status(HttpStatus.OK.value()).build());
+
+        verify(elinksResponsesRepository, times(0))
+                .save(any());
+
+    }
+
+    @Test
+    void saveElinksResponsesSuccess() throws JsonProcessingException {
+
+        List<BaseLocationResponse> baseLocations = getBaseLocationResponseData();
+
+        ElinkBaseLocationResponse elinkBaseLocationResponse = new ElinkBaseLocationResponse();
+        elinkBaseLocationResponse.setResults(baseLocations);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String body = mapper.writeValueAsString(elinkBaseLocationResponse);
+
+        elinksResponsesHelper.saveElinksResponse(LOCATION, Response.builder()
+                .request(mock(Request.class)).body(body, defaultCharset()).status(HttpStatus.OK.value()).build());
+
+        verify(elinksResponsesRepository, times(0))
+                .save(any());
+
+    }
+
+    @Test
+    void saveElinksResponsesFailure() throws JsonProcessingException {
+        Assertions.assertThrows(IllegalStateException.class, () -> elinksResponsesHelper
+                    .saveElinksResponse(LOCATION, Response.builder().build()));
+    }
+
+
+    private List<BaseLocationResponse> getBaseLocationResponseData() {
+        BaseLocationResponse baseLocationOne = new BaseLocationResponse();
+        baseLocationOne.setId("1");
+        baseLocationOne.setName("Aberconwy");
+        baseLocationOne.setTypeId("46");
+        baseLocationOne.setParentId("1722");
+        baseLocationOne.setTypeId("28");
+        baseLocationOne.setCreatedAt("2023-04-12T16:42:35Z");
+        baseLocationOne.setUpdatedAt("2023-04-12T16:42:35Z");
+
+        BaseLocationResponse baseLocationTwo = new BaseLocationResponse();
+
+        baseLocationTwo.setId("2");
+        baseLocationTwo.setName("Aldridge and Brownhills");
+        baseLocationTwo.setTypeId("48");
+        baseLocationTwo.setParentId("1723");
+        baseLocationTwo.setTypeId("29");
+        baseLocationTwo.setCreatedAt("2023-04-12T16:42:35Z");
+        baseLocationTwo.setUpdatedAt("2023-04-12T16:42:35Z");
+
+        return List.of(baseLocationOne,baseLocationTwo);
+
+    }
+
+
+
+
+
+}
