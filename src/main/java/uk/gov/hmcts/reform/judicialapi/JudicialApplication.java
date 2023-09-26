fb9be2549d344ac586bd422b1295f5263d07c8ff
Andy Boyle <Andrew.Boyle@HMCTS.NET>
2023-09-26 15:17:56 +0100

    DTSRD-89

--- src/main/java/uk/gov/hmcts/reform/judicialapi/JudicialApplication.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/JudicialApplication.java
@@ -1,16 +1,26 @@
 package uk.gov.hmcts.reform.judicialapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 
-@SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform.judicialapi")
+@SpringBootApplication(scanBasePackages = {
+        "uk.gov.hmcts.reform.judicialapi",
+        "uk.gov.hmcts.reform.idam"
+})
 @EnableFeignClients(basePackages = { "uk.gov.hmcts.reform.judicialapi" },
-        basePackageClasses = { IdamApi.class, ServiceAuthorisationApi.class }
-)
+        basePackageClasses = {
+        IdamApi.class,
+        ServiceAuthorisationApi.class
+})
+@ImportAutoConfiguration({
+        org.springframework.cloud.openfeign.FeignContext.class,
+        org.springframework.cloud.openfeign.FeignClientProperties.class
+})
 @EnableCaching
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class JudicialApplication {
