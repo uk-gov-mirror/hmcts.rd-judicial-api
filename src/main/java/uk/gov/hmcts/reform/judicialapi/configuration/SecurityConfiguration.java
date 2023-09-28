f86d105891c7435e1b802e2f5904222ba5e884b2
Andy Boyle <Andrew.Boyle@HMCTS.NET>
2023-09-28 13:35:23 +0100

    DTSRD-89

--- src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/SecurityConfiguration.java
@@ -6,8 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
-import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
