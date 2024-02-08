6004bd5ff649f6daef51d35151cc84c271d70063
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2024-02-08 11:29:10 +0000

    DTSAM-132: Enable 2 new authorisation for Magistrate access in Private Law Jurisdiction (#747)

---
+++ src/main/resources/db/migration/V1_95__elinks_insert_service_code_mapping.sql
@@ -0,0 +1,7 @@
+INSERT INTO dbjudicialdata.judicial_service_code_mapping (service_id,ticket_code,service_code,service_description) VALUES
+    ('127','1453','ABA5','Family Private Law');
+
+INSERT INTO dbjudicialdata.judicial_service_code_mapping (service_id,ticket_code,service_code,service_description) VALUES
+    ('128','405','ABA5','Family Private Law');
+
+COMMIT;
