a9396bba764d7f9c04d4b0b32e8359cf48484bfa
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-03-07 11:34:26 +0000

    Schema Owner Fix - RETRIGER SCRIPT

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -99,7 +99,7 @@ module "db-rd-judicial-ref-v16" {
 
   # Sets correct DB owner after migration to fix permissions
   enable_schema_ownership = var.enable_schema_ownership
-  force_schema_ownership_trigger = "1"
+  force_schema_ownership_trigger = "2"
   kv_subscription = var.kv_subscription
   kv_name = data.azurerm_key_vault.rd_key_vault.name
   user_secret_name = azurerm_key_vault_secret.POSTGRES-USER.name
