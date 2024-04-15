01b88de20bbe77c726966fa4a239a200737901a6
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2024-04-15 11:53:09 +0100

    Create user role if not exists (#771)
    * DTSRD-2525: Upgrade chat version
    
    * DTSRD-2525: create dbReaderUserName if not exists
    
    * DTSRD-2525: create dbReaderUserName if not exists
    
    * Bumping chart version/ fixing aliases
    
    * DTSRD-2525: change chart version
    
    ---------
    
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>

---
+++ src/main/resources/db/migration/V1_97__elinks_grant_access_dbjudicialdata.sql
@@ -0,0 +1,23 @@
+-- Grant usage and select privs on Reader user
+
+DO
+$do$
+BEGIN
+IF EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = '${dbReaderUserName}') THEN
+
+RAISE NOTICE 'Role "${dbReaderUserName}" already exists. Skipping.';
+ELSE
+BEGIN
+CREATE ROLE "${dbReaderUserName}" WITH LOGIN IN ROLE azure_ad_user;
+EXCEPTION
+WHEN duplicate_object THEN
+RAISE NOTICE 'Role "${dbReaderUserName}" was just created by a concurrent transaction. Skipping.';
+END;
+END IF;
+END
+$do$;
+
+GRANT USAGE ON SCHEMA dbjudicialdata TO "${dbReaderUserName}";
+GRANT SELECT ON ALL TABLES IN SCHEMA dbjudicialdata TO "${dbReaderUserName}";
