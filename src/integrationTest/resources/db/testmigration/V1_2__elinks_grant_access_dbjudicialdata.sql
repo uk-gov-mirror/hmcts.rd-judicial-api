aca5d47a482df5f0450d6e180e91754175c9016e
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-05-07 14:15:15 +0100

    DTSRD-2029: eLinks : Post-Production rollout for Strategic solution : CHG5014213

--- src/integrationTest/resources/db/testmigration/V1_2__elinks_grant_access_dbjudicialdata.sql
+++
@@ -1,3 +0,0 @@
--- Grant usage and select privs on Reader user
-GRANT USAGE ON SCHEMA dbjudicialdata TO "${dbReaderUserName}";
-GRANT SELECT ON ALL TABLES IN SCHEMA dbjudicialdata TO "${dbReaderUserName}";
