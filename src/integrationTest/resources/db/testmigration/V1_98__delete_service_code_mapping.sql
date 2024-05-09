d730693917d69b8e503ff5e192b79a98951bbb45
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-05-09 19:06:06 +0100

    DTSRD-2029: eLinks : Post-Production rollout for Strategic solution : CHG5014213

---
+++ src/integrationTest/resources/db/testmigration/V1_98__delete_service_code_mapping.sql
@@ -0,0 +1,5 @@
+DELETE  FROM judicial_service_code_mapping where ticket_code = '376' and service_code = 'BBA2';
+
+DELETE  FROM dbjudicialdata.judicial_service_code_mapping where ticket_code = '376' and service_code = 'BBA2';
+
+COMMIT;
