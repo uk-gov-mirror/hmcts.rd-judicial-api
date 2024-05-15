2a42f1608e37ed91fdc31ada0c4db31fb16f0a92
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-05-15 18:46:39 +0100

    Decomissioning single server

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,23 +1,29 @@
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
+
 provider "azurerm" {
   features {}
   skip_provider_registration = true
   alias                      = "postgres_network"
   subscription_id            = var.aks_subscription_id
 }
+
 terraform {
   backend "azurerm" {}
 
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.101.0"
+      version = "~> 3.103.1"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.33.0"
+      version = "2.48.0"
     }
   }
 }
