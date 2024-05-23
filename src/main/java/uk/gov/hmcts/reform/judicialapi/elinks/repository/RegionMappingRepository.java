4ebadb1a971161ed3250d21852268e97c5797e8d
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-05-23 16:03:39 +0100

    Remove redundant class

--- src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/repository/RegionMappingRepository.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.judicialapi.elinks.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import uk.gov.hmcts.reform.judicialapi.elinks.domain.RegionMapping;
-
-@Repository("elinksRegionMappingRepository")
-public interface RegionMappingRepository extends JpaRepository<RegionMapping, String> {
-}
