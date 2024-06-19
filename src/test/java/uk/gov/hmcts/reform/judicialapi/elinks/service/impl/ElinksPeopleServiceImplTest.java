c3ba67ec422b7fda6c21e1b9498b2659b0d78e97
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-06-19 14:35:46 +0100

    DTSRD-2900: Email Validation change and location name stripping

--- src/test/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/ElinksPeopleServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/ElinksPeopleServiceImplTest.java
@@ -879,6 +879,8 @@ class ElinksPeopleServiceImplTest {
         verify(elinksFeignClient, times(1)).getPeopleDetails(any(), any(), any(),
                 Boolean.parseBoolean(any()));
         verify(profileRepository, times(1)).save(any());
+        verify(appointmentsRepository, times(1)).save(any());
+
     }
 
 
