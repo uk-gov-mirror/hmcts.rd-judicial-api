6029cbb2f0377641bedc5c967e2b095ad8963868
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-06-26 13:43:05 +0100

    DTSRD-2900: Fix alter table alter column issue

--- src/integrationTest/resources/db/testmigration/V1_101__alter_column_appointment.sql
+++ src/integrationTest/resources/db/testmigration/V1_101__alter_column_appointment.sql
@@ -1,1 +1,1 @@
-ALTER TABLE dbjudicialdata.judicial_office_appointment ALTER COLUMN appointment TYPE VARCHAR(256) NOT NULL;
+ALTER TABLE dbjudicialdata.judicial_office_appointment ALTER COLUMN appointment TYPE VARCHAR(256);
