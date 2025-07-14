758409c01f856e886cda85380476d31d16da75ce
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2025-07-14 10:57:22 +0100

    Update TestTopicPublishController.java

--- src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/TestTopicPublishController.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/TestTopicPublishController.java
@@ -126,5 +126,4 @@ public class TestTopicPublishController {
             throw new ElinksException(HttpStatus.FORBIDDEN, JOB_DETAILS_UPDATE_ERROR, JOB_DETAILS_UPDATE_ERROR);
         }
     }
-        
 }
