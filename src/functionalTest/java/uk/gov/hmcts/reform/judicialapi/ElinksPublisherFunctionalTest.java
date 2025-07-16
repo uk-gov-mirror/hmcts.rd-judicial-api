83ad014136894c7bfe13f928805cfa382e901d2c
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2025-07-16 11:05:44 +0100

    Update ElinksPublisherFunctionalTest.java

--- src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/ElinksPublisherFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/ElinksPublisherFunctionalTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.reform.judicialapi.elinks.servicebus.ElinkTopicPublisher;
 import uk.gov.hmcts.reform.judicialapi.elinks.util.ElinkDataIngestionSchedularAudit;
