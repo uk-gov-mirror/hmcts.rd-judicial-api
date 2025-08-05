79642f324f85779e7375a7451e1faf0e7d670a79
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-08-05 13:14:24 +0100

    DTSRD-4072-RecreateError ..

--- src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/ElinksPublisherFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/ElinksPublisherFunctionalTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.judicialapi;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -45,12 +46,21 @@ class ElinksPublisherFunctionalTest  {
 
         // Given a list of ids from judicial that will be sent to the topic
         String jobId = UUID.randomUUID().toString();
-        List<String> userIds = List.of("integration-user-1", "integration-user-2",
+        /*List<String> userIds = List.of("integration-user-1", "integration-user-2",
             "integration-user-3","integration-user-4","integration-user-5","integration-user-6","integration-user-7",
             "integration-user-8","integration-user-9","integration-user-10","integration-user-11",
             "integration-user-12","integration-user-13","integration-user-14","integration-user-15",
             "integration-user-16","integration-user-17","integration-user-18","integration-user-19",
-            "integration-user-20");
+            "integration-user-20");*/
+
+        List<String> userIds = new ArrayList<>(4000);
+
+        for (int i = 1; i <= 4000; i++) {
+            userIds.add("integration-user-" + i);
+        }
+
+        // Example: print the first 10 user IDs
+        userIds.stream().limit(10).forEach(System.out::println);
 
         // When
         publisher.sendMessage(userIds, jobId);
