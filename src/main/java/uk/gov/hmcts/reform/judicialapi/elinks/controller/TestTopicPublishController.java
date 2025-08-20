ce58f6c9cb131580006cb73c6c8650d2a5e65294
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-08-20 13:08:24 +0100

    DTSRD-4072-add new functional test delete old

--- src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/TestTopicPublishController.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/TestTopicPublishController.java
@@ -20,7 +20,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
