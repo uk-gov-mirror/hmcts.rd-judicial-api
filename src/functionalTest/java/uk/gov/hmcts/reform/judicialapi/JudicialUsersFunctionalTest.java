628b6c0a477363cf499dec4e5dd1df882d425868
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2025-08-29 08:58:59 +0100

    Update JudicialUsersFunctionalTest.java

--- src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/JudicialUsersFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/JudicialUsersFunctionalTest.java
@@ -123,7 +123,7 @@ class JudicialUsersFunctionalTest extends AuthorizationFunctionalTest {
 
         List<String> userIds = new ArrayList<>();
 
-        for (int i = 1; i < 20257; i++) {
+        for (int i = 1; i < 200257; i++) {
             userIds.add("SADE-Judicial-user-785fa62b-3e79-415b-bfc4-1ad83d95a73b" + i);
         }
 
@@ -138,7 +138,7 @@ class JudicialUsersFunctionalTest extends AuthorizationFunctionalTest {
         assertEquals(OK.value(), publishResponse.getStatusCode());
         String expected = "{\n"
             + "    \"statusCode\": 200,\n"
-            + "    \"sidamIdsCount\": 19256,\n"
+            + "    \"sidamIdsCount\": 200256,\n"
             + "    \"id\": \"1234\",\n"
             + "    \"publishing_status\": \"SUCCESS\"\n"
             + "}";
