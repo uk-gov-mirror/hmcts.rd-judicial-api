04622789daf86dff050831cb7091ad445fdd0cca
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-09-01 16:30:32 +0100

    DTSRD-4072 committing changes to testcase

--- src/test/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/TestTopicPublishControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/TestTopicPublishControllerTest.java
@@ -15,14 +15,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import uk.gov.hmcts.reform.judicialapi.elinks.controller.request.RefreshRoleRequest;
 import uk.gov.hmcts.reform.judicialapi.elinks.response.SchedulerJobStatusResponse;
-import uk.gov.hmcts.reform.judicialapi.elinks.service.PublishSidamIdService;
 import uk.gov.hmcts.reform.judicialapi.elinks.servicebus.ElinkTopicPublisher;
 
 @ExtendWith(MockitoExtension.class)
